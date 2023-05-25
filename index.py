0d7b314a50f69a2a265015801188315ae5341eb2
Alberto Júnior <ajfiumanee@outlook.com>
2023-05-25 14:13:56 -0300

    [fix] - Finalization model

--- index.py
+++ index.py
@@ -1,11 +1,7 @@
 # Imports
 import pandas as pd
 import numpy as np
-import matplotlib
 import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn import svm
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import ConfusionMatrixDisplay
 
@@ -46,27 +42,7 @@ dataset['Location Y Axis'] = dataset['Location Y Axis'].apply(lambda x: str(x).r
 # primeiras linhas do dataset
 # print(dataset.head())
 
-# Separação em conjuntos de treino e teste
-array = dataset.values
-columnsTemp = dataset.columns
-X = dataset.drop(['General Health', 'Atopy',
-                  'Hypertension', 'Hayfever', 'Known Eye History',
-                  'Family History KC', 'Primary Optical Aid', 'Eye', 'Sphere', 'Cylinder', 'Axis',
-                  'Flat Keratometry', 'Steep Keratometry', 'Steep Axis',
-                  'Topography Cylinder', 'Central Pachy',
-                  'Thinnest pachy', 'Location X Axis', 'Location Y Axis'], axis=1)
-Y = dataset["label"]
-
-# clf = svm.SVC(decision_function_shape='ovo')
-# clf.fit(X, Y)
-# dec = clf.decision_function(X)
-# print(dec.shape)
-#
-# clf = svm.SVC(decision_function_shape='ovr')
-# clf.fit(X, Y)
-# dec = clf.decision_function(X)
-# print(dec.shape)
-
+# Estágios
 stages = dataset.values[:, dataset.columns.size - 1:dataset.columns.size].astype(int)
 # print(str(stages))
 
@@ -77,27 +53,43 @@ stages = dataset.values[:, dataset.columns.size - 1:dataset.columns.size].astype
 
 ###########
 
-test_size = 0.20
-seed = 21
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=test_size, random_state=seed, shuffle='true')
+# Separação em conjuntos de treino e teste
+array = dataset.values
+
+X = dataset.drop(['General Health', 'Atopy',
+                  'Hypertension', 'Hayfever', 'Known Eye History',
+                  'Family History KC', 'Primary Optical Aid', 'Eye', 'Sphere', 'Cylinder', 'Axis',
+                  'Flat Keratometry', 'Steep Keratometry', 'Steep Axis',
+                  'Topography Cylinder', 'Central Pachy',
+                  'Thinnest pachy', 'Location X Axis', 'Location Y Axis'], axis=1)
+Y = dataset["label"]
 
 # Parâmetros
+test_size = 0.20
+seed = 21
+seed_global = 7
 num_folds = 12
 num_jobs = 2
 scoring = 'accuracy'
 solver = 'newton-cg'
 max_interactions = 10000
 
+###########
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=test_size, random_state=seed, shuffle='true')
+
 # Criação dos modelos
 models = [
-    ('LR', LogisticRegression(solver=solver, max_iter=max_interactions, random_state=seed)),
-    ('KNN', KNeighborsClassifier(n_neighbors=num_folds, n_jobs=num_jobs)),
+    ('LR', LogisticRegression(solver=solver)),
+    ('KNN', KNeighborsClassifier()),
     ('NB', GaussianNB()),
     ('SVM', SVC())
 ]
 
+###########
+
 # definindo uma semente global
-np.random.seed(seed)
+np.random.seed(seed_global)
 
 # Avaliação dos modelos
 results = []
@@ -117,23 +109,27 @@ print('')
 
 # Comparação dos modelos
 fig = plt.figure()
-fig.suptitle('Comparação dos Modelos')
+# fig.suptitle('Comparação dos Modelos')
 ax = fig.add_subplot(111)
 plt.boxplot(results)
 ax.set_xticklabels(names)
-# plt.show()
+plt.show()
 
 print('')
+
+###########
 
 pipelinesResults = []
 pipelinesNames = []
 
 # Padronização do dataset
-pipelines = []
-pipelines.append(('ScaledLR', Pipeline([('Scaler', StandardScaler()), ('LR', LogisticRegression())])))
-pipelines.append(('ScaledKNN', Pipeline([('Scaler', StandardScaler()), ('KNN', KNeighborsClassifier())])))
-pipelines.append(('ScaledNB', Pipeline([('Scaler', StandardScaler()), ('NB', GaussianNB())])))
-pipelines.append(('ScaledSVM', Pipeline([('Scaler', StandardScaler()), ('SVM', SVC())])))
+
+np.random.seed(seed_global)
+
+pipelines = [('Scaled LR', Pipeline([('Scaler', StandardScaler()), ('LR', LogisticRegression())])),
+             ('Scaled KNN', Pipeline([('Scaler', StandardScaler()), ('KNN', KNeighborsClassifier())])),
+             ('Scaled NB', Pipeline([('Scaler', StandardScaler()), ('NB', GaussianNB())])),
+             ('Scaled SVM', Pipeline([('Scaler', StandardScaler()), ('SVM', SVC())]))]
 
 print('Pipelines:')
 for name, model in pipelines:
@@ -141,26 +137,31 @@ for name, model in pipelines:
     cv_results = cross_val_score(model, X_train, Y_train.ravel(), cv=kFoldItem, scoring=scoring)
     pipelinesResults.append(cv_results)
     pipelinesNames.append(name)
-    msg = "%s: %f" % (name, cv_results.mean())
+    msg = "%s: %f (% f)" % (name, cv_results.mean(), cv_results.std())
     print(msg)
 
 print('')
 
 # Comparação dos Pipeline
 fig = plt.figure()
-fig.suptitle('Comparação do Pipeline')
+# fig.suptitle('Comparação do Pipeline')
 ax = fig.add_subplot(111)
 plt.boxplot(pipelinesResults)
 ax.set_xticklabels(pipelinesNames)
-# plt.show()
+plt.show()
 
 print('')
 
-# Tuning do KNN
-scaler = StandardScaler().fit(X_train)
-rescaledX = scaler.transform(X_train)
+###########
 
-k = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21]
+# Tuning do KNN
+
+np.random.seed(seed_global)
+
+# scaler = StandardScaler().fit(X_train)
+# rescaledX = scaler.transform(X_train)
+
+k = [3, 5, 7, 9, 11, 13, 15, 17, 19, 21]
 metrics = ["euclidean", "manhattan", "minkowski"]
 param_grid = dict(n_neighbors=k, metric=metrics)
 
@@ -168,22 +169,29 @@ model = KNeighborsClassifier()
 kFold = KFold(n_splits=num_folds)
 
 grid = GridSearchCV(estimator=model, param_grid=param_grid, scoring=scoring, cv=kFold, n_jobs=num_jobs, verbose=1)
-grid_result = grid.fit(rescaledX, Y_train)
+grid_result = grid.fit(X_train, Y_train)
 
+print('KNN')
 print("Melhor: % f usando % s" % (grid_result.best_score_, grid_result.best_params_))
 
 print('')
-means = grid_result.cv_results_['mean_test_score']
-stds = grid_result.cv_results_['std_test_score']
-params = grid_result.cv_results_['params']
-for mean, stdev, param in zip(means, stds, params):
-    print(" % f( % f): % r" % (mean, stdev, param))
+# means = grid_result.cv_results_['mean_test_score']
+# stds = grid_result.cv_results_['std_test_score']
+# params = grid_result.cv_results_['params']
+# for mean, stdev, param in zip(means, stds, params):
+#     print(" % f( % f): % r" % (mean, stdev, param))
+#
+# print('')
 
-print('')
+###########
+
+np.random.seed(seed_global)
+
+scaler = StandardScaler().fit(X_train)
+rescaledX = scaler.transform(X_train)
 
 # Tuning do SVM
-c_values = [1, 2, 3, 4, 5]
-# c_values = [4]
+c_values = [0.1, 0.5, 1.0, 1.5, 2.0]
 kernel_values = ['linear', 'poly', 'rbf', 'sigmoid']
 param_grid = dict(C=c_values, kernel=kernel_values)
 
@@ -191,32 +199,48 @@ model = SVC()
 kFold = KFold(n_splits=num_folds, random_state=seed, shuffle=True)
 grid = GridSearchCV(estimator=model, param_grid=param_grid, scoring=scoring, cv=kFold)
 grid_result = grid.fit(rescaledX, Y_train)
+print('SVM')
 print("Melhor: %f com %s" % (grid_result.best_score_, grid_result.best_params_))
 
-# print('')
+print('')
 # means = grid_result.cv_results_['mean_test_score']
 # stds = grid_result.cv_results_['std_test_score']
 # params = grid_result.cv_results_['params']
 # for mean, stdev, param in zip(means, stds, params):
 #     print("%f (%f): %r" % (mean, stdev, param))
+#
+# print('')
+
+###########
+
+np.random.seed(seed_global)
+
+# Preparação do modelo
+
+scaler = StandardScaler().fit(X)
+X_standardized = scaler.transform(X)
+
+X_train_S, X_test_S, y_train_S, y_test_S = train_test_split(X_standardized, Y, test_size=test_size, random_state=seed,
+                                                            shuffle='true', stratify=Y)
+
+
+# model = LogisticRegression(solver=solver, random_state=seed)
+model = SVC(C=0.5, random_state=seed, kernel='linear')
+
+model.fit(X_train_S, y_train_S)
+
+# Estimativa da acurácia no conjunto de teste
+predictions = model.predict(X_test_S)
+print("Accuracy score = ", accuracy_score(y_test_S, predictions))
 
 print('')
 
-# Preparação do modelo
-model = LogisticRegression(solver='newton-cg')
-model.fit(X_train, Y_train)
-
-# Estimativa da acurácia no conjunto de teste
-predictions = model.predict(X_test)
-# print("Accuracy score = ", accuracy_score(Y_test, predictions))
-
-# print('')
-
 # Matriz de confusão
-cm = confusion_matrix(Y_test, predictions)
+cm = confusion_matrix(y_test_S, predictions)
 labels = ["1", "2", "3", "4"]
 cmd = ConfusionMatrixDisplay(cm, display_labels=labels)
-# cmd.plot(values_format="d")
-# plt.suptitle('Matriz de confusão')
-# plt.show()
-print(classification_report(Y_test, predictions, target_names=labels))
+cmd.plot(values_format="d")
+plt.suptitle('Matriz de confusão - SVM')
+plt.show()
+
+print(classification_report(y_test_S, predictions, target_names=labels, zero_division=0))
